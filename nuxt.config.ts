e0be27e217572b61f3f06cd9a11a79a579c93e8d
Mieles Alvarez Diego Jose <133074732+iwachunaruchi@users.noreply.github.com>
2024-10-20 02:35:58 -0500

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    'nuxt-translation-manager',
+    'nuxt-auth-sanctum',
+    '@nuxtjs/color-mode',
+  ],
+
+})
