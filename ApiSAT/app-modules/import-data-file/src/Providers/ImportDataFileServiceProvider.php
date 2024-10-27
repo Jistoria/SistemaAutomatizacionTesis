3dc146a7d9dfb54bee92c72e8d3e43e51a612619
Jistoria <maestrojgcm@gmail.com>
2024-10-27 09:31:44 -0500

    Iniciando modulo, se piensa usar python para facilitar el formateo del pdf

---
+++ ApiSAT/app-modules/import-data-file/src/Providers/ImportDataFileServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\ImportDataFile\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImportDataFileServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
