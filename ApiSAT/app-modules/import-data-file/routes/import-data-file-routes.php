95857e43a9410073814de5614cbf10409f2b44b0
Johan Cañizares <maestrojgcm@gmail.com>
2024-10-27 19:01:40 -0500

    Se va la luz pipipi

--- ApiSAT/app-modules/import-data-file/routes/import-data-file-routes.php
+++ ApiSAT/app-modules/import-data-file/routes/import-data-file-routes.php
@@ -1,11 +1,6 @@
 <?php
 
-// use Modules\ImportDataFile\Http\Controllers\ImportDataFileController;
+use Illuminate\Support\Facades\Route;
+use Modules\ImportDataFile\Http\Controllers\ImportDataFileController;
 
-// Route::get('/import-data-files', [ImportDataFileController::class, 'index'])->name('import-data-files.index');
-// Route::get('/import-data-files/create', [ImportDataFileController::class, 'create'])->name('import-data-files.create');
-// Route::post('/import-data-files', [ImportDataFileController::class, 'store'])->name('import-data-files.store');
-// Route::get('/import-data-files/{import-data-file}', [ImportDataFileController::class, 'show'])->name('import-data-files.show');
-// Route::get('/import-data-files/{import-data-file}/edit', [ImportDataFileController::class, 'edit'])->name('import-data-files.edit');
-// Route::put('/import-data-files/{import-data-file}', [ImportDataFileController::class, 'update'])->name('import-data-files.update');
-// Route::delete('/import-data-files/{import-data-file}', [ImportDataFileController::class, 'destroy'])->name('import-data-files.destroy');
+Route::post('import-data-file', [ImportDataFileController::class, 'importDataFile']);
---
+++ ApiSAT/app-modules/import-data-file/src/Http/Controllers/ImportDataFileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\ImportDataFile\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImportDataFileController
+{
+    public function importDataFile(Request $request)
+    {
+        // Importar archivo de datos
+    }
+}
