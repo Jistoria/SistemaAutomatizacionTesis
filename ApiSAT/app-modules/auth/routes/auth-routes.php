12673613433c51d5fd7319a80c79ab5960172ed6
Jistoria <maestrojgcm@gmail.com>
2024-10-29 18:53:42 -0500

    aplicando CI pipelines

--- ApiSAT/app-modules/auth/routes/auth-routes.php
+++ ApiSAT/app-modules/auth/routes/auth-routes.php
@@ -5,7 +5,7 @@ use Modules\Auth\Http\Controllers\AuthController;
 
 Route::prefix('auth')->group(
     function () {
-        Route::post('login', [AuthController::class, 'login'])->middleware('guest:api');
+        Route::post('login', [AuthController::class, 'login'])->middleware(['guest:api', 'throttle:6,1']);
         Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
         Route::get('user', [AuthController::class, 'user'])->middleware('auth:api');
     }
