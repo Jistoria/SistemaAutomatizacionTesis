feac1e712565969aa29cfc7b57e06695d1f584c1
Jistoria <maestrojgcm@gmail.com>
2024-11-13 15:53:57 -0500

    agregando migraciones y models de observaciones para requisitos y fase del estudiante

---
+++ ApiSAT/database/migrations/2024_11_13_200022_create_observations_requirements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('observations_requirements', function (Blueprint $table) {
+            $table->uuid('observation_requirement_id')->primary();
+            $table->text('comment')->nullable();
+            $table->uuid('created_by_user');
+            $table->uuid('student_requirements_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign keys
+            $table->foreign('created_by_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('student_requirements_id')->references('id')->on('student_requirements')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('observations_requirements');
+    }
+};
---
+++ ApiSAT/database/migrations/2024_11_13_200342_create_observations_phases_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('observations_phases', function (Blueprint $table) {
+            $table->uuid('observation_phase_id')->primary();
+            $table->text('comment')->nullable();
+            $table->uuid('created_by_user');
+            $table->uuid('thesis_process_phases_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign keys
+            $table->foreign('created_by_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('thesis_process_phases_id')->references('id')->on('thesis_process_phases')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('observations_phases_students');
+    }
+};
