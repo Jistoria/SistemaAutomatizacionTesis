023c5c446586b7af6a6a326bdcf27182a676b8aa
Jistoria <maestrojgcm@gmail.com>
2024-11-22 10:41:45 -0500

    agregando supervisor, ruta para confirmar que redis trabaje y comando para levantar la queue

--- ApiSAT/routes/web.php
+++ ApiSAT/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/redis-test', function () {
+    \Illuminate\Support\Facades\Redis::set('test', 'It works!');
+    return \Illuminate\Support\Facades\Redis::get('test');
+});
