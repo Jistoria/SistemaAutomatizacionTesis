719492b5787dd8ced455d9de7a387ffdd9dad5b6
Alexander Meneses <alexmeneses2004@gmail.com>
2024-12-05 18:42:56 -0500

    docker

--- FrontSAT/plugins/echo.client.ts
+++ FrontSAT/plugins/echo.client.ts
@@ -10,8 +10,8 @@ window.Pusher = Pusher;
 const broadcaster = import.meta.env.VITE_BROADCASTER;
 const key = import.meta.env.VITE_KEY;
 const wsHost = import.meta.env.VITE_WS_HOST;
-const wsPort = import.meta.env.VITE_WS_PORT;
-const wssPort = import.meta.env.VITE_WSS_PORT;
+// const wsPort = import.meta.env.VITE_WS_PORT;
+// const wssPort = import.meta.env.VITE_WSS_PORT;
 
 // console.log('Broadcaster:', broadcaster);
 // console.log('Key:', key);
@@ -24,11 +24,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const echo = new Echo({
     broadcaster: broadcaster, // Usa el broadcaster de Reverb
     key: key,// Usa la clave de la variable de entorno
-    wsHost: wsHost,
-    wsPort: wsPort,
-    //wssPort: wssPort,
-    forceTLS: false,
-    enabledTransports: ['ws'],
+    wsHost:'',
+    wssPort:'',
+    forceTLS: true,
+    enabledTransports: ['wss'],
     disableStats: true,
   });
 
